5e2523180287757ac76669cb7f5561b0c7e711f9
Ketlyn Santos <ketlyncastro2004@gmail.com>
2026-06-04 12:16:55 -0300

    feat: adiciona biblioteca leaflet

---
+++ js/data.js
@@ -0,0 +1,162 @@
+const COMMUNITIES = [
+    {
+        name: "Rio Verde",
+        inhabitants: 1240,
+        distanceHospital: "85 km",
+        connectivity: "Via satélite",
+        risks: [
+            "Doenças respiratórias",
+            "Hipertensão"
+        ],
+        activeUnit: "ORB-02",
+        lat: -3.119,
+        lng: -60.021
+    },
+    {
+        name: "Nova Esperança",
+        inhabitants: 860,
+        distanceHospital: "120 km",
+        connectivity: "Via satélite",
+        risks: [
+            "Gestação de risco",
+            "Diabetes"
+        ],
+        activeUnit: "ORB-05",
+        lat: -2.443,
+        lng: -54.708
+    },
+    {
+        name: "São Lucas",
+        inhabitants: 540,
+        distanceHospital: "150 km",
+        connectivity: "Via satélite",
+        risks: [
+            "Acidentes de trabalho"
+        ],
+        activeUnit: "ORB-03",
+        lat: -9.974,
+        lng: -67.824
+    }
+];
+
+const EMERGENCIES = [
+    {
+        community: "Rio Verde",
+        type: "Emergência respiratória",
+        priority: "alta",
+        lat: -3.109,
+        lng: -60.011
+    },
+    {
+        community: "Nova Esperança",
+        type: "Gestante de risco",
+        priority: "alta",
+        lat: -2.443,
+        lng: -54.708
+    },
+    {
+        community: "São Lucas",
+        type: "Acidente de trabalho",
+        priority: "alta",
+        lat: -9.974,
+        lng: -67.824
+    }
+];
+
+const UNITS = [
+    // Rio Verde
+    {
+        id: "ORB-01",
+        status: "atendimento",
+        community: "Rio Verde",
+        lat: -3.110,
+        lng: -60.015
+    },
+    {
+        id: "ORB-02",
+        status: "atendimento",
+        community: "Rio Verde",
+        lat: -3.129,
+        lng: -60.031
+    },
+    {
+        id: "ORB-03",
+        status: "deslocamento",
+        community: "Rio Verde",
+        lat: -3.125,
+        lng: -60.030
+    },
+    {
+        id: "ORB-04",
+        status: "retorno",
+        community: "Rio Verde",
+        lat: -3.130,
+        lng: -60.010
+    },
+
+    // Nova Esperança
+    {
+        id: "ORB-05",
+        status: "atendimento",
+        community: "Nova Esperança",
+        lat: -2.443,
+        lng: -54.708
+    },
+    {
+        id: "ORB-06",
+        status: "atendimento",
+        community: "Nova Esperança",
+        lat: -2.450,
+        lng: -54.700
+    },
+    {
+        id: "ORB-07",
+        status: "deslocamento",
+        community: "Nova Esperança",
+        lat: -2.435,
+        lng: -54.715
+    },
+    {
+        id: "ORB-08",
+        status: "retorno",
+        community: "Nova Esperança",
+        lat: -2.455,
+        lng: -54.720
+    },
+
+    // São Lucas
+    {
+        id: "ORB-09",
+        status: "atendimento",
+        community: "São Lucas",
+        lat: -9.974,
+        lng: -67.824
+    },
+    {
+        id: "ORB-10",
+        status: "atendimento",
+        community: "São Lucas",
+        lat: -9.980,
+        lng: -67.818
+    },
+    {
+        id: "ORB-11",
+        status: "deslocamento",
+        community: "São Lucas",
+        lat: -9.968,
+        lng: -67.832
+    },
+    {
+        id: "ORB-12",
+        status: "retorno",
+        community: "São Lucas",
+        lat: -9.985,
+        lng: -67.840
+    }
+];
+
+window.Mock = {
+  UNITS,
+  COMMUNITIES,
+  EMERGENCIES
+}
---
+++ js/map.js
@@ -0,0 +1,179 @@
+// Localização Brasil
+const map = L.map('map').setView([-14.2350, -51.9253], 4);
+const unitIcon = L.divIcon({
+    className: "custom-marker",
+    html: "🚐",
+    iconSize: [30, 30]
+});
+
+const communityIcon = L.divIcon({
+    className: "custom-marker",
+    html: "🏘️",
+    iconSize: [30, 30]
+});
+
+const emergencyIcon = L.divIcon({
+    className: "custom-marker",
+    html: "🚨",
+    iconSize: [30, 30]
+});
+
+let selectedCommunityArea = null;
+
+L.tileLayer(
+    'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+    { attribution: '&copy; OpenStreetMap' }
+).addTo(map);
+
+document
+    .getElementById("resetMap")
+    .addEventListener("click", () => {
+
+        if (selectedCommunityArea) {
+            map.removeLayer(selectedCommunityArea);
+            selectedCommunityArea = null;
+        }
+
+        map.flyTo(
+            [-14.2350, -51.9253],
+            4,
+            { duration: 1.5 }
+        );
+    });
+
+Mock.UNITS.forEach(unit => {
+    L.marker(
+        [unit.lat, unit.lng],
+        { icon: unitIcon }
+    )
+    .addTo(map)
+    .bindPopup(`
+        <div class="popup">
+            <div class="popup__header u-border-bottom u-padding-horizontal-bottom">
+                <h3 class="popup__title">${unit.id}</h3>
+                <span class="popup__status popup__status--${unit.status}">${unit.status}</span>
+            </div>
+            <div class="popup__infos">
+                <div class="popup__info">
+                    <span class="material-symbols-rounded popup__icon">location_on</span>
+                    <span>${unit.community}</span>
+                </div>
+                <div class="popup__info">
+                    <span class="material-symbols-rounded popup__icon">emergency</span>
+                    <span>Unidade Móvel de Saúde</span>
+                </div>
+            </div>
+        </div>
+    `);
+});
+
+Mock.EMERGENCIES.forEach(emergency => {
+    L.marker(
+        [emergency.lat, emergency.lng],
+        { icon: emergencyIcon }
+    )
+    .addTo(map)
+    .bindPopup(`
+        <div class="popup">
+            <div class="popup__header u-border-bottom u-padding-horizontal-bottom">
+                <h3 class="popup__title">${emergency.type}</h3>
+                <span class="popup__badge popup__badge--danger">Crítica</span>
+            </div>
+            <div class="popup__infos">
+                <div class="popup__info">
+                    <span class="material-symbols-rounded popup__icon">location_on</span>
+                    <span>${emergency.community}</span>
+                </div>
+                <div class="popup__info">
+                    <span class="material-symbols-rounded popup__icon">warning</span>
+                    <span>Atendimento prioritário</span>
+                </div>
+            </div>
+        </div>
+    `);
+});
+
+
+Mock.COMMUNITIES.forEach(community => {
+    const marker = L.marker(
+        [community.lat, community.lng],
+        { icon: communityIcon }
+    );
+
+
+    marker.bindPopup(`
+        <div class="popup">
+            <div class="popup__header u-border-bottom u-padding-horizontal-bottom">
+                <h3 class="popup__title">${community.name}</h3>
+                <span class="popup__badge popup-badge--warning">Prioritária</span>
+            </div>
+            <div class="popup__infos u-border-bottom u-padding-horizontal-bottom">
+                <div class="popup__info">
+                    <span class="material-symbols-rounded popup__icon">groups</span>
+                    <span>${community.inhabitants.toLocaleString()} habitantes</span>
+                </div>
+
+                <div class="popup__info">
+                    <span class="material-symbols-rounded popup__icon">local_hospital</span>
+                    <span>${community.distanceHospital}</span>
+                </div>
+
+                <div class="popup__info">
+                    <span class="material-symbols-rounded popup__icon">satellite_alt</span>
+                    <span>${community.connectivity}</span>
+                </div>
+
+                <div class="popup__info">
+                    <span class="material-symbols-rounded popup__icon">emergency</span>
+                    <span>${community.activeUnit}</span>
+                </div>
+            </div>
+            <div class="popup__risks">
+                <h4 class="popup__subtitle">Principais acometimentos</h4>
+                ${community.risks.map(risk =>`<span class="popup__tag">${risk}</span>`).join("")}
+            </div>
+        </div>
+    `);
+
+    marker.on("click", () => {
+        // Remove destaque anterior
+        if (selectedCommunityArea) {
+            map.removeLayer(selectedCommunityArea);
+            selectedCommunityArea = null;
+        }
+
+        // Cria novo destaque
+        selectedCommunityArea = L.circle(
+            [community.lat, community.lng],
+            { 
+                radius: getCommunityRadius(community.inhabitants),
+                color: "#3d8d80",
+                weight: 3,
+                fillColor: "#89d2ad",
+                fillOpacity: 0.25
+            }
+        ).addTo(map);
+
+        const bounds = selectedCommunityArea.getBounds();
+        map.flyToBounds(bounds, {
+            padding: [20, 20],
+            duration: 1.5
+        });
+
+        marker.openPopup();
+    });
+
+    marker.addTo(map);
+});
+
+function getCommunityRadius(inhabitants) {
+    if (inhabitants < 700) {
+        return 12000;
+    }
+
+    if (inhabitants < 1000) {
+        return 18000;
+    }
+
+    return 25000;
+}
