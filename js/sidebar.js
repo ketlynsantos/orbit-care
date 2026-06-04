0248a2ebd6f92908f575d3359cbad2c374db55d8
Ketlyn Santos <ketlyncastro2004@gmail.com>
2026-06-04 12:17:27 -0300

    feat: adiciona modo 'open' ao sidebar para dispositivos móveis

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,6 +1,17 @@
 const sidebar = document.querySelector(".sidebar");
 const menuToggle = document.getElementById("menuToggle");
+const backdrop = document.querySelector(".sidebar-backdrop");
 
 menuToggle.addEventListener("click", () => {
-    sidebar.classList.toggle("sidebar--collapsed");
+    if (window.innerWidth <= 768) {
+        sidebar.classList.toggle("sidebar--opened");
+        backdrop.classList.toggle("sidebar-backdrop--active");
+    } else {
+        sidebar.classList.toggle("sidebar--collapsed");
+    }
+});
+
+backdrop.addEventListener("click", () => {
+    sidebar.classList.remove("sidebar--opened");
+    backdrop.classList.remove("sidebar-backdrop--active");
 });
